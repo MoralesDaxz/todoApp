ab65e63cf554a1793df959f1e3c3a4d66d871a07
moralesdev <alfredojose.92@gmail.com>
2026-08-12 14:17:46 +0200

    feat: enhance ToDo page layout and functionality with improved task input and navigation

--- src/pages/ToDo.page.tsx
+++ src/pages/ToDo.page.tsx
@@ -1,23 +1,20 @@
-import { useParams } from "react-router";
+import { Link, useParams } from "react-router";
 import { useAuth } from "../context/AuthContext";
 import { useState } from "react";
 import { useTodos } from "../features/todos/hooks/useTodos";
 import { useLists } from "../features/todos/hooks/useLists";
 
-type TodoStatus = "confirmed" | "pending" | "done_by_user";
+
 
 export const ToDo = () => {
   const { listId } = useParams<{ listId: string }>();
   const { user } = useAuth();
-  const { todos, isLoading, addMutation, markAsDoneMutation, confirmMutation } = useTodos(listId || null);
+  const { todos, isLoading, addMutation, markAsDoneMutation, confirmMutation } =
+    useTodos(listId || null);
   const { lists } = useLists();
   const [newTaskText, setNewTaskText] = useState("");
 
-  const statusIcons: Record<TodoStatus, string> = {
-    confirmed: "✅",
-    pending: "⌛",
-    done_by_user: "✋🏻",
-  };
+
 
   const handleAddTask = () => {
     if (!newTaskText.trim() || !listId || !user) return;
@@ -30,45 +27,68 @@ export const ToDo = () => {
   };
 
   // Corrección vital: usar ?. para evitar que la app se rompa si lists aún no tiene datos
-  const listName = lists.find(item => item.id === listId)?.name?.toUpperCase() || "CARGANDO...";
+  const listName =
+    lists.find((item) => item.id === listId)?.name?.toUpperCase() ||
+    "CARGANDO...";
 
   if (isLoading) return <div>Cargando...</div>;
 
   return (
     <div className="w-full p-2">
+      <Link className="absolute top-1 left-2" to={"/dashboard"}>
+        Volver
+      </Link>
       <h1>{listName}</h1>
-      
-      <input
-        value={newTaskText}
-        onChange={(e) => setNewTaskText(e.target.value)}
-        placeholder="Añadir tarea..."
-      />
-      <button onClick={handleAddTask}>Añadir</button>
+
+      <div className="flex gap-1 my-3 border-2 border-gray-400 rounded-sm">
+        <input
+          className="outline-none p-2 text-xl"
+          value={newTaskText}
+          onChange={(e) => setNewTaskText(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleAddTask()}
+          placeholder="Añadir tarea..."
+        />
+        <button
+          className="bg-blue-400 text-white rounded-r-xs text-sm p-3 font-medium"
+          onClick={handleAddTask}
+        >
+          Añadir
+        </button>
+      </div>
 
       <ul>
         {todos.map((todo) => {
           // Evaluamos el estado AQUÍ, para cada "todo" individual
-          const statusMsg = statusIcons[todo.status as TodoStatus] || "❓";
 
           return (
-            <li className="my-4 border-2 border-gray-400 rounded-md flex gap-4 items-center" key={todo.id}>
-              <span className="text-sm text-gray-400">{statusMsg}</span>
-              <span className="flex-1">{todo.task}</span>
-              
-              {todo.status === "pending" && (
-                <button 
-                  className="bg-blue-600 text-white rounded-md"
+            <li
+              className="my-1 border-2 border-gray-600 rounded-sm flex gap-2 items-center"
+              key={todo.id}
+            >
+              {/* <span className="text-sm text-gray-400">{statusMsg}</span> */}
+              <span className="ml-1 flex-1">{todo.task}</span>
+              {todo.status === "confirmed" && (
+                <button
+                  className="bg-gray-500 text-white opacity-90 text-sm font-medium p-2 rounded-r-xs"
                   onClick={() => markAsDoneMutation.mutate(todo.id)}
                 >
-                 {statusMsg} Realizado
+                  Hecho
+                </button>
+              )}
+              {todo.status === "pending" && (
+                <button
+                  className="bg-yellow-500 text-white text-sm font-medium p-2 rounded-r-xs"
+                  onClick={() => markAsDoneMutation.mutate(todo.id)}
+                >
+                  Aprobar
                 </button>
               )}
               {todo.status === "done_by_user" && (
-                <button 
-                  className="bg-yellow-600 text-white rounded-md"
+                <button
+                  className="bg-green-500 text-white text-sm font-medium p-2 rounded-r-xs"
                   onClick={() => confirmMutation.mutate(todo.id)}
                 >
-                 {statusMsg} Confirmar
+                  Confirmar
                 </button>
               )}
             </li>
